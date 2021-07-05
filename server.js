fe78affdab4a1e5d6a169703c02c7d7460e19505
Samantha Malone <sammac99@gmail.com>
2021-07-05 12:48:33 -0700

    added route for animal data

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 // initiating express server
 const express = require('express');
 const app = express();
+// route to request data
+const { animals } = require('./data/animals.json');
+app.get('/api/animals', (req, res) => {
+    res.send('Hello!');
+  });
 // chain listen method
 app.listen(3001, () => {
     console.log(`API server now on port 3001!`);
