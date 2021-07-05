1e92e418a94464997b74b4dd2c43b94e04d836d7
Samantha Malone <sammac99@gmail.com>
2021-07-05 15:44:28 -0700

    adding POST and Insomnia tests

--- server.js
+++ server.js
@@ -67,3 +67,9 @@ app.get('/api/animals/:id', (req, res) => {
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
   });
+// add animals to catalog
+app.post('/api/animals', (req, res) => {
+  //req.body is where our incoming content will be
+  console.log(req.body);
+  res.json(req.body);
+});
