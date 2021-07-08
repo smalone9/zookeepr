d7c48eb8f8a61abb39868f2468185b23486d0c54
Samantha Malone <sammac99@gmail.com>
2021-07-07 17:11:14 -0700

    api animal routes modularized

--- server.js
+++ server.js
@@ -14,37 +14,6 @@ app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
 app.use(express.json());
 
-app.get('/api/animals', (req, res) => {
-    let results = animals;
-    if (req.query) {
-      results = filterByQuery(req.query, results);
-    }
-    res.json(results);
-  });
-
-app.get('/api/animals/:id', (req, res) => {
-    const result = findById(req.params.id, animals);
-    if (result) {
-        res.json(result);
-      } else {
-        res.send(404);
-      }
-  });
-
-// add animals to catalog
-app.post('/api/animals', (req, res) => {
-  // set id based on what the next index of the array will be
-  req.body.id = animals.length.toString();
-
-  // if any data in req.body is incorrect, send 400 error back
-  if (!validateAnimal(req.body)) {
-    res.status(400).send('The animal is not properly formatted.');
-  } else {
-    const animal = createNewAnimal(req.body, animals);
-    res.json(animal);
-  }
-});
-
 // route to front end homepage
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, './public/index.html'))
